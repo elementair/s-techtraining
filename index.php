71fc37a93d1feb80ca97b040247cbbc59bdea8f0
emanuel arias <element.earias20@gmail.com>
2018-10-08 00:59:44 -0500

    version 1.0

---
+++ index.php
@@ -0,0 +1,394 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>S-techtraining</title>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Estilos -->
+<!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">-->
+    <link rel="stylesheet" href="bootstrap/dist/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/menu.css">
+    <link rel="stylesheet" href="css/inicio.css">
+    <link rel="stylesheet" href="css/style.css">
+
+
+
+<!--    slider temporal-->
+
+<!--    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
+    <script src="bootstrap/dist/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+    <script src="js/jquery.flexisel.js"></script>
+    <script src="js/parallax.js"></script>
+
+
+
+
+<!--    Java Script-->
+
+
+
+
+
+
+  
+
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="50">
+    <!--====================================================
+                             MENU
+    ====================================================-->
+<nav id="navbar" class="navbar navbar-inverse navbar-fixed-top">
+    <div class="nav_superior">
+        <p>(33)5555 5555  &nbsp; &nbsp; <b>|</b>  &nbsp;  &nbsp; ventas@s-techtraining.com</p>
+        <div class="icono_leng">
+            <img src="img/logo/es.png" alt="">ES
+            <img src="img/logo/en.png" alt="">EN
+        </div>
+
+    </div>
+  <div class="menu"> 
+  <div class="container-fluid">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>                        
+      </button>
+      <!-- <a class="navbar-brand" href="#">WebSiteName</a> -->
+      <a class="navbar-brand" href="#" id="logo"><img src="img/logo/logo_sinectech_training.svg"></a>
+    </div>
+    <div>
+      <div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav">
+<!--            <li><a class="navbar-brand" href="#" id="logo"><img src="img/logo/logo_sinectech_training.svg" style="width: 100%"></a></li>-->
+            <li><a href="#inicio">INICIO</a></li>
+            <li><a href="#servicios">SERVICIOS</a></li>
+            <li><a href="#galeria">GALERIA</a></li>
+            <li><a href="#contacto">CONTACTO</a></li>
+            <li class="iconos">
+
+            <a href=""><img src="img/iconos/icono_facebook.svg" alt=""></a>
+            <a href=""><img src="img/iconos/icono_twitter.svg" alt=""></a>
+            </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  </div>
+</nav>
+
+<!--====================================================
+                       FIN MENU
+====================================================-->
+
+<!--====================================================
+                       INICIO
+====================================================-->
+
+<section id="inicio" class="container-fluid">
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+                <div class="item active">
+                    <div class="img_inicio" style="background-image: url('img/img_index.jpg')"></div>
+                </div>
+                <div class="item">
+                    <div class="img_inicio" style="background-image: url('img/img_index_dos.jpg')"></div>
+                </div>
+                <div class="item">
+                    <div class="img_inicio" style="background-image: url('img/img_index_tres.jpg')"></div>
+
+                </div>
+                <p>"Pasión <br>
+                por la educación"</p>
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+</section>
+
+
+<!--====================================================
+                       FIN INICIO
+====================================================-->
+<!--====================================================
+                       NOSOTROS
+====================================================-->
+<section id="nosotros" class="container-fluid">
+    <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-6 img_nosotros">
+            <img src="img/galeria/1.jpg" alt="">
+        </div>
+        <div class="col-md-6  txt_titulo">
+            <h3>NUESTRA META</h3>
+            <div class="col-md-5 pleca"></div>
+            <p>Somos una compañía dedicada a la mejora de calidad y competitividad de nuestros clientes en la industria electrónica a nivel mundial.</p>
+        </div>
+    </div>
+    <div class="img_banda_circuito" ">
+        <img src="img/banda_circuits.jpg" alt="">
+
+    </div>
+    <div class="col-md-8 col-md-offset-2">
+
+        <div class="col-md-6 txt_titulo">
+            <h3>ENTRENAMIENTO/EDUCACIN</h3>
+            <div class="col-md-5 pleca"></div>
+            <p>Son los elementos fundamentales necesarios para obtener calidad. Estos elementos están sujetos y medidos no solo en el despeño del producto, sino también en la moral y productividad del personal.</p>
+        </div>
+        <div class="col-md-6 img_nosotros2">
+            <img src="img/galeria/7.jpg" alt="">
+        </div>
+    </div>
+</section>
+
+<!--====================================================
+                       FIN NOSOTROS
+====================================================-->
+<!--====================================================
+                       OBJETIVO
+====================================================-->
+<section id="objetivo" class="container-fluid">
+
+    <div class="col-md-10 col-md-offset-1">
+        <div class="col-md-6 img_objetivo">
+            <img src="img/motherboards.jpg" alt="">
+        </div>
+        <div class="col-md-6 txt_objetivo">
+
+            <div class="col-md-12 titulo2">
+                <p><font color="#fff"></font>OBJETIVOS</p>
+                <div class="col-md-4 pleca"></div>
+            </div>
+
+            <p>Los cursos que impartimos nos basamos en la Formación de nuestros participantes a través de Entrenamiento y lo que es mas importante la educación son los elementos fundamentales necesarios para obtener calidad y productividad del personal mejor preparado, consciente y pro-activa en su área</p>
+        </div>
+    </div>
+
+</section>
+<!--====================================================
+                       FIN OBJETIVO
+====================================================-->
+
+
+<!--====================================================
+                         SERVICIOS
+====================================================-->
+<section id="servicios" class="container-fluid">
+    <div class="col-md-10 col-md-offset-1">
+        <div class="col-md-12 titulo">
+            <p>SERVICIOS</p>
+            <div class="col-md-2 col-md-offset-5 pleca"></div>
+        </div>
+        <div class="col-xs-12 col-md-4 ">
+            <div class=" cursos"><p>CURSOS</p></div>
+        </div>
+        <div class="col-xs-12 col-md-4 ">
+            <div class=" certificaciones"><p>CERTIFICACIONES</p></div>
+        </div>
+        <div class="col-xs-12 col-md-4 ">
+            <div class=" cursos_especializados"><p>CURSOS ESPECIALIZADOS</p></div>
+        </div>
+    </div>
+</section>
+
+
+<!--====================================================
+                       FIN SERVICIOS
+====================================================-->
+
+<!--====================================================
+                         GALERIA
+====================================================-->
+
+<section id="galeria" class="">
+    <div class="container-fluid">
+    <div class="col-md-12 titulo">
+        <p>GALERIA</p>
+    </div>
+
+
+
+    <ul id="flexiselDemo3">
+        <li><img src="img/galeria/1.jpg" /></li>
+        <li><img src="img/galeria/2.jpg" /></li>
+        <li><img src="img/galeria/3.jpg" /></li>
+        <li><img src="img/galeria/4.jpg" /></li>
+        <li><img src="img/galeria/5.jpg" /></li>
+        <li><img src="img/galeria/6.jpg" /></li>
+        <li><img src="img/galeria/7.jpg" /></li>
+    </ul>
+    </div>
+
+
+</section>
+<div class="background_gray">
+
+<!--====================================================
+                       FIN GALERIA
+====================================================-->
+<!--====================================================
+                       CONTACTO
+====================================================-->
+<section id="contacto" class="container-fluid">
+
+    <div class="col-md-10 col-md-offset-1">
+        <div class="col-md-12 titulo">
+            <p>CONTACTO</p>
+
+        </div>
+        <div class="col-md-6 formulario">
+            <div class="col-md-12 txt_titulo">
+                <h4>ENVIANOS UN MENSAJE</h4>
+            </div>
+            <form action="">
+                <div class="form-group col-md-5">
+                    <input type="text" class="form-control" placeholder="Nombre*">
+                </div>
+                <div class="form-group col-md-5">
+                    <input type="text" class="form-control" placeholder="Email*">
+                </div>
+                <div class="form-group col-md-10">
+                    <input class="form-control" type="text" placeholder="Empresa*">
+                </div>
+                <div class="form-group col-md-10 ">
+                    <textarea class="form-control" rows="7" id="comment" placeholder="Mensaje"></textarea>
+                </div>
+                <div class="form-group col-md-8 col-offset-2 ">
+                    <button type="button" class="btn btn-default btn-md">ENVIAR</button>
+                </div>
+            </form>
+        </div>
+        <div class="col-md-6 mapa">
+            <div class="col-md-10 subtitulo2">
+                <h2>Ubicación</h2>
+                <p>Parque industrial Nogales 5797
+                <br> Colonia Campo Lago
+                <br> Zapopan, jal. México
+                <br> C.P. 45222</p>
+
+            </div>
+            <div class="col-md-10 mapa">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3731.9347322497442!2d-103.47828658574959!3d20.71287480386428!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8428a8e7c1139761%3A0x5102ca503dda3b26!2sCarr+a+Nogales+5797%2C+Campo+Lago%2C+Zapopan%2C+Jal.!5e0!3m2!1sen!2smx!4v1538633784096" width="400" height="200" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+            </div>
+        </div>
+
+
+
+    </div>
+
+
+</section>
+
+<!--====================================================
+                     FIN CONTACTO
+====================================================-->
+
+
+<footer>
+    <hr>
+    <center> <p> © sinetechtraining 2018</p></center>
+</footer>
+
+</div>
+
+<script>
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "15px 0px";
+
+  } else {
+    document.getElementById("navbar").style.padding = "20px 0px";
+  }
+}
+</script>
+    <script type="text/javascript">
+
+        $(window).load(function() {
+            $("#flexiselDemo1").flexisel();
+
+            $("#flexiselDemo2").flexisel({
+                visibleItems: 4,
+                itemsToScroll: 3,
+                animationSpeed: 200,
+                infinite: true,
+                navigationTargetSelector: null,
+                autoPlay: {
+                    enable: false,
+                    interval: 5000,
+                    pauseOnHover: true
+                },
+                responsiveBreakpoints: {
+                    portrait: {
+                        changePoint:480,
+                        visibleItems: 1,
+                        itemsToScroll: 1
+                    },
+                    landscape: {
+                        changePoint:640,
+                        visibleItems: 2,
+                        itemsToScroll: 2
+                    },
+                    tablet: {
+                        changePoint:768,
+                        visibleItems: 3,
+                        itemsToScroll: 3
+                    }
+                },
+                loaded: function(object) {
+                    console.log('Slider loaded...');
+                },
+                before: function(object){
+                    console.log('Before transition...');
+                },
+                after: function(object) {
+                    console.log('After transition...');
+                },
+                resize: function(object){
+                    console.log('After resize...');
+                }
+            });
+
+            $("#flexiselDemo3").flexisel({
+                visibleItems: 5,
+                itemsToScroll: 1,
+                autoPlay: {
+                    enable: true,
+                    interval: 10000,
+                    pauseOnHover: true
+                }
+            });
+
+            $("#flexiselDemo4").flexisel({
+                infinite: false
+            });
+
+        });
+    </script>
+
+
+
+</body>
+</html>
